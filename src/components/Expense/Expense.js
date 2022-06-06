7d342c3db3a30ad4b9d41034af5a5a944e28f454
Enrico Rombaldoni <e.rombaldoni@teamsystem.com>
2022-06-06 15:47:41 +0200

    feat: mysolution compito2

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -1,9 +1,16 @@
 import './css/Expense.css';
+import React, { useState } from 'react';
 import ExpenseDate from './items/ExpenseDate';
 import ExpenseItem from './items/ExpenseItem';
+import ExpensesFilter from './items/ExpensesFilter';
 
 function Expense(props) {
     const render = [];
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const FilterByYears = (data) => {
+       setFilteredYear(data.value);
+    };
+
     props.data.forEach((data, i) => {
       render.push(
        <div className='expense-item' key={i}>
@@ -14,6 +21,7 @@ function Expense(props) {
       })
   return (
     <>
+      <ExpensesFilter  selcted={filteredYear} ChangeYearFather={FilterByYears} />
       {render}
     </>
   );
