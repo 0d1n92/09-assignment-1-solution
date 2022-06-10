d2d2b81fd2d5bdc6c095489cc694f40c4d90b313
0d1n92 <rombaldonienrico@gmail.com>
2022-06-10 12:26:09 +0200

    filter by year

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -6,14 +6,22 @@ import ExpensesFilter from './items/ExpensesFilter';
 
 function Expense(props) {
     const [filteredYear, setFilteredYear] = useState('2020');
+    const [allDates, setAllDates] = useState(props.data);
+    const item = props.data;
     const FilterByYears = (data) => {
        setFilteredYear(data.value);
     };
 
+   const onChangeDate = (year) => {
+      setAllDates(props.data);
+      const results = item.filter((element)=> element.date.getFullYear().toString() === year);
+      setAllDates(results);
+   };
+
   return (
     <>
-      <ExpensesFilter  selcted={filteredYear} ChangeYearFather={FilterByYears} />
-      {props.data.map((data, i) => (
+      <ExpensesFilter  selected={filteredYear} onChangeDate={onChangeDate} ChangeYearFather={FilterByYears} />
+      {allDates.map((data, i) => (
         <div className='expense-item' key={i}>
           <ExpenseDate date={data.date}></ExpenseDate>
           <ExpenseItem title={data.title} amount={data.amount}></ExpenseItem>
